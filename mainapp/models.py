0fe661dbc1a97289f497ac851562f2179f2916be
dviralafi <dviralafiprogram@gmail.com>
2022-06-26 03:31:19 +0300

    add MidiFile Model for User

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,20 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from mainapp.s3_methods import get_presigned_url_of_file
+
+
+class MidiFile(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=False,related_name="midi_files")
+    file_name = models.CharField(max_length=300)
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+    def get_presigned_url(self):
+        return get_presigned_url_of_file(self.file_name)
+
+    def __str__(self):
+        return self.file_name
+    class Meta:
+        ordering = ["creation_date"]
+
+
