b776cb8805a5dbfb2b5f788040d268f09e65e9a4
dviralafi <dviralafiprogram@gmail.com>
2022-06-28 23:28:42 +0300

    debug 500 internal error

--- RandomMelodySite/settings.py
+++ RandomMelodySite/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv("RM_SITE_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 # True - UnDeployed
 # False - Deployed
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1","https://random-melody-site.herokuapp.com/"]
 
