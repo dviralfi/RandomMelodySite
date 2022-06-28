a1b4f95d6babe39e74096ac33fb86078c57b68a4
dviralafi <dviralafiprogram@gmail.com>
2022-06-28 23:46:19 +0300

    fix syntax error

--- mainapp/views.py
+++ mainapp/views.py
@@ -153,8 +153,8 @@ def generatemidifile(request,*args,**kwargs):
             if not scale_key: scale_key = choice(CHROMATIC_KEYS)
             if not scale_type: scale_type = choice(list(SCALES_DICT.keys()))
                 
-            temp_file_name = str(request.META)+['REMOTE_ADDR']+"PC_RandomMelody.mid"
-            
+            temp_file_name = str(request.META['REMOTE_ADDR'])+"_PC_RandomMelody.mid"
+
             # File Creation : (Create file in MIDIFILES_PATH in heroku/local machine (depends if deployed))
             file_path =  random_melody_generator.main(
                 file_name=temp_file_name,
